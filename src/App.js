f359b40284d2ea34b18ba14115ace99f12d65704
Omkar Dhumal <omkardhumal213@gmail.com>
2022-07-13 10:59:05 +0530

    intailized repo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import './App.css';
-import Users1 from './Users1';
-import UserFn from './UserFn';
-import UselayoutEx from '../src/Components/UselayoutEx'
-import Useeffect from './Components/Useeffect';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 
 function App() {
+  
   return (
     <>
-      {/* <Users1/> */}
-      {/* <UserFn/> */}
-      {/* <UselayoutEx/> */}
-      <Useeffect/>
+      <Router>
+        
+      </Router>
     </>
   );
 }
